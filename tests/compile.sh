8e6f3aeae4e706f7d6e94182ba4d512b5d0edd3b
CollinseyNyaga <monsieurcollmut@gmail.com>
2022-01-06 16:33:45 +0300

    added the shell scripts for compilation

--- tests/compile.sh
+++ tests/compile.sh
@@ -1,1 +1,10 @@
-# this script compiles the test program into executable. 
+#/bin/bash
+# this script takes the mds library file and test.cs files and compiles test program into executable. 
+
+execName="test"
+
+mcs test.cs ../mds.cs -out:./build/"${execName}.exe"
+
+printf ""
+printf ""
+printf "Run the bashscript : run.sh"
