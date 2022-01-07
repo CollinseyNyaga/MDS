7050b7cce74f53961af49d78275a925a3b88e6eb
CollinseyNyaga <monsieurcollmut@gmail.com>
2022-01-07 20:06:21 +0300

    another comm today

--- tests/test.cs
+++ tests/test.cs
@@ -11,14 +11,26 @@ namespace test
     {
         static void Main()
         {
+            // test0();
             test1();
         }
 
+
+        // 
+
+        static void test0()
+        {
+            System.Console.WriteLine("hello data structure");
+        }
         static void test1()
         {
             // adding an element to the universal container : 
-            
-            System.Console.WriteLine("hello data structure");
+            string fname = "mutant";
+            string lname = "mutate";
+
+            Mds.Ucont myArray = new Mds.Ucont(); 
+            myArray.AddElement(fname);      
+            myArray.AddElement(lname);     
         }
 
     }
