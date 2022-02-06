23eea11ed2cfa4d596feedc723e64b0ff813db70
CollinseyNyaga <monsieurcollmut@gmail.com>
2022-02-06 22:40:50 +0300

    road to fix the contains method

--- mds.cs
+++ mds.cs
@@ -54,18 +54,23 @@ namespace Mds
 
         public bool Contains(string element)
         {
-            for (uint i = 0; i <= (uint)Size; i++)
+            for (int i = 0; i >= 5; i++)
             {
                 System.Console.WriteLine(i);
-
-                // check if the element is contained in the container in a reverse order.
-                if (GetElement(index: i) == element)
-                {
-                    return true;
-                }
             }
 
-            return false;
+                // for (uint i = 0; i <= (uint)Size; i++)
+                // {
+                //     System.Console.WriteLine(i);
+
+                //     // check if the element is contained in the container in a reverse order.
+                //     if (GetElement(index: i) == element)
+                //     {
+                //         return true;
+                //     }
+                // }
+
+                return false;
         }
 
 
