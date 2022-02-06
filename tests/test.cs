c9ceeb9943fb8b2c74e1a54792ed85708d6e09dd
CollinseyNyaga <monsieurcollmut@gmail.com>
2022-02-06 22:24:31 +0300

    Crapp ,, the GetElement method is not working well lol

--- tests/test.cs
+++ tests/test.cs
@@ -78,7 +78,7 @@ namespace test
             // System.Console.WriteLine(A);
             // System.Console.WriteLine(B);
 
-            System.Console.WriteLine(mycont.GetElement(index:2));
+            System.Console.WriteLine(mycont.GetElement(index:1));
         }
 
     }
